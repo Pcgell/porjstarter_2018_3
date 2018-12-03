6aaeabc354fc7b804affa0d5fcd854e8995e6309
Carlos <cmchamorro99@hotmail.com>
2018-12-03 08:50:48 -0600

    Primer commit

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -3,6 +3,7 @@ package edu.ujcv.progra1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world");
+        System.out.println();
     }
 }
