8b5ceb095a5d9cca6e9a19dd02b0c75f653a72d1
kenneth vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2018-12-07 08:36:26 -0600

    Agregando codigo del menu

--- src/edu/ujcv/progra1/Menu/Menu.java
+++ src/edu/ujcv/progra1/Menu/Menu.java
@@ -1,7 +1,34 @@
 package edu.ujcv.progra1.Menu;
 
+import edu.ujcv.progra1.util.LectorTeclado;
+
 import java.util.ArrayList;
 
 public class Menu {
     private ArrayList<IMenuItem> items;
-}
+
+
+    public Menu(ArrayList<IMenuItem> items) {
+        this.items = items;
+    }
+
+
+    public void mostrarMenu(){
+
+        int opcion = 1;
+        for (IMenuItem menuItem: items) {
+            System.out.println(""+ opcion++ + ". "+menuItem.getLabel());
+        }
+    }
+
+
+    public void leerOpcionLuegoProcessar(){
+        LectorTeclado lt = new LectorTeclado();
+        int opcion =0;
+        while(opcion<=0 || opcion > items.size())
+        opcion = lt.leerEntero("ingrese una opcion valida","vuelva intentar");
+
+        items.get(opcion-1).runAction();
+
+    }
+ }
